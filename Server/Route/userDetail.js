baf6ae0dcb640682e8fcdb7435a1ae8d0472193e
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-01-30 20:54:10 +0530

    Remove bycrpt algo

--- Server/Route/userDetail.js
+++ Server/Route/userDetail.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userSchema from '../Module/userSchema.js'
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 const apiRouter = express.Router()
 
@@ -11,11 +11,11 @@ apiRouter.post('/createUserProfile', async (req, res) => {
         if (findProfile.length !== 0) {
             return res.send({"error":"Profile is already Created, please login"})
         }
-        const { name, email, password } = req.body
-        const saltRounds = 10
-        const salt = await bcrypt.genSalt(saltRounds);
-        const hashedPassword = await bcrypt.hash(password, salt);
-        const createUser = await userSchema.create({ name, email, password: hashedPassword })
+        const { name, email} = req.body
+        // const saltRounds = 10
+        // const salt = await bcrypt.genSalt(saltRounds);
+        // const hashedPassword = await bcrypt.hash(password, salt);
+        const createUser = await userSchema.create({ name, email})
         res.send(createUser)
     } catch (error) {
         res.status(500).send('Network Error')
@@ -25,15 +25,15 @@ apiRouter.post('/createUserProfile', async (req, res) => {
 apiRouter.post('/login', async (req, res) => {
     try {
         const userEmail = req.body.email
-        const userPassword = req.body.password
+        // const userPassword = req.body.password
         const findEmail = await userSchema.findOne({ email: userEmail })
         if (!findEmail) {
             return res.send({"error":"Invalid Email ID details"})
         }
-        const decryptPassword = await bcrypt.compare(userPassword, findEmail.password)
-        if (!decryptPassword) {
-            return res.send({"error":"Invalid User details"})
-        }
+        // const decryptPassword = await bcrypt.compare(userPassword, findEmail.password)
+        // if (!decryptPassword) {
+        //     return res.send({"error":"Invalid User details"})
+        // }
         let jwtSecretKey = process.env.JWT_SECRET_KEY;
         const token = jwt.sign({ email: userEmail }, jwtSecretKey);
         res.send({
